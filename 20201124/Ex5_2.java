767edcd3a744fe5408e537c4cb872b72027634ae
hanamoto <sadasosonohara@docomo.ne.jp>
2020-11-24 14:41:35 +0900

    まとめて追加

---
+++ 20201124/Ex5_2.java
@@ -0,0 +1,10 @@
+public class Ex5_2{
+	public static void main(String[] args){
+		email("例の件","hoge@gmail.com","例の件です");
+	}
+	static void email(String title,String address,String text){
+		System.out.println(address + "に、以下のメールを送信しました");
+		System.out.println("件名:"+title);
+		System.out.println("本文:"+text);
+	}
+}
---
+++ 20201124/hiroseLesson1.java
@@ -0,0 +1,5 @@
+import java.util.*;
+public class hiroseLesson1{
+	public static void main(String[] args){
+	}
+}
