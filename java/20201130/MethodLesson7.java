246ca6cc69668e501630e8129999a01889cfc5dc
hanamoto <sadasosonohara@docomo.ne.jp>
2020-11-30 14:48:19 +0900

    追加

---
+++ java/20201130/MethodLesson7.java
@@ -0,0 +1,10 @@
+public class MethodLesson7{
+	static int sum0f(int a,int b){
+		return a+b;
+	}
+	public static void main(String[] args){
+		int sum=sum0f(3,5);
+		System.out.println(sum);
+		System.out.println(sum0f(10,20));
+	}
+}
---
+++ java/20201130/MethodLesson9.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class MethodLesson8{
+	static double calcBMI(double weightkg,double heightCm){
+		double height=heightCm/100;
+		double bmi=weightKg/(heightM*heightM);
+		return bmi;
+	}
+	public static void main(String[] args){
+		Scanner scan=new Scanner(System.in);
+		System.out.print("体重を入力してください(kg)>");
+		double weight=scan.nextDouble();
+		System.out.print("身長を入力してください(cm)>");
+		double height=scan.nextDouble();
+		double bmi=calcBMI(weight,height);
+		System.out.printf("体重:%.1fkg,身長:%.1fcmのBMIは%.1fです。",weight,height,bmi);
+		scan.close();
+	}
+}
+
