d2543f6d7b9617cbec950e0fc048cf3ac711ac07
hanamoto <sadasosonohara@docomo.ne.jp>
2020-12-09 14:36:13 +0900

    追加

--- java/20201207/Archer.java
+++ java/20201207/Archer.java
@@ -1,8 +1,15 @@
-public class Archer{
+public class Archer {
 	private static int totalArcher;
 	private int no;
 	public Archer(){
+	 totalArcher++;	
+	 this.no=totalArcher;
+	 System.out.println("Archer"+this.no+"‚ğ¶¬‚µ‚Ü‚µ‚½B");
+	}
+	public void shootArrow(){
+		System.out.println("Archer"+this.no+"‚Í–î‚ğ•ú‚Á‚½I");
 	}
 	public static int getTotalArcher(){
+		return totalArcher;
 	}
 }
--- java/20201207/ArcherApp.java
+++ java/20201207/ArcherApp.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
-public static int getTotalArcher(){
-	public static void main(String[] args){
-		while(true){
-		System.out.println("操作を入力してください1…Archer生成,2…Archerの総数確認,3…終了>");
-		int select=new Scanner(System.in).nextInt();
-		switch(select){
-case1:
-			int 
+public class ArcherApp2 {
+	public static void main(String[] args) {
+		Archer[] archers=new Archer[10];
+		for(int i=0;i<archers.length;i++){
+			archers[i]=new Archer();
 		}
-		}
+		System.out.print("アーチャー番号を入力(1~10):");
+		Scanner scan=new Scanner(System.in);
+		int no=scan.nextInt();
+		archers[no-1].shootArrow();
+		scan.close();
 	}
 }
